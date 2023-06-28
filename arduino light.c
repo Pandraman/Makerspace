dd8a46a3c4ffd1de1bd99f58e80b1dac339aa54f
Pandraman <92603994+Pandraman@users.noreply.github.com>
2023-06-28 17:30:58 +0200

    Update arduino light.c

--- arduino light.c
+++ arduino light.c
@@ -5,19 +5,20 @@
 int LERpins[2] = {A1, A0};
 int LEDpins[2] = {13, 12};
 
-int counterA;
-int counterB;
-
 int sensorA;
 int sensorB;
 int umgebung;
 int timeSinceGoal;
 
+int counterA;
+int counterB;
+
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 
-
+bool Init;
 void setup() {
+  Init = false;
   for(int i; i<*(&LERpins+1)-LERpins; i++){pinMode(LERpins[i], INPUT);}
   for(int i; i<*(&LEDpins+1)-LEDpins; i++){pinMode(LEDpins[i], OUTPUT);}
 
@@ -27,17 +28,7 @@ void setup() {
   digitalWrite(13, HIGH);
 
   Serial.begin(9600);
-  umgebung = digitalRead(LERpins[A];
-}
-
-void read(){
-  if (LERpins[A] < umgebung/3) {
-    if (timeSinceGoal > 1000) {
-      counterA += 1; timeSinceGoal = 0;
-      lcd.clear();
-      write(counterA, counterB);
-    }
-  }
+  
 }
 
 void write(int cA, int cB) {
@@ -49,14 +40,37 @@ void write(int cA, int cB) {
   lcd.print(cB);
 }
 
+int read(int pin, int counter){
+
+    if (analogRead(pin) < umgebung/2) {
+      if (timeSinceGoal > 500) {
+        timeSinceGoal = 0;
+        lcd.clear();
+       return (counter+1);
+       }
+    }
+  return (counter);
+}
+// folter für vale
+
+
 void checkreset(){
   if (counterA > 9) {counterA = 0;}
   if (counterB > 9) {counterB = 0;}
 }
 
 void loop() {
-  read();
+  if (!Init) {
+    umgebung = analogRead(LERpins[A]);
+    Init = true;
+  }
+
+
+  
+ counterA = read(LERpins[A], counterA);
+ counterB = read(LERpins[B], counterB);
+ write(counterA, counterB);
   checkreset();
   timeSinceGoal++;
-  Serial.println(analogRead(LERpins[A]));
+  Serial.println((analogRead(LERpins[A]) < umgebung/2));
 }
