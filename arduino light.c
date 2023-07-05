10e3f65c0e4345e49a8e332cd2d40115535b683c
Pandraman <92603994+Pandraman@users.noreply.github.com>
2023-07-05 15:43:06 +0200

    Juli 05
    2 LCDs.

--- arduino light.c
+++ arduino light.c
@@ -13,39 +13,45 @@ int timeSinceGoal;
 int counterA;
 int counterB;
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-
+LiquidCrystal_I2C lcdA(0x27, 16, 2);
+LiquidCrystal_I2C lcdB(0x26, 16, 2);
 
 bool Init;
 void setup() {
   Init = false;
-  for(int i; i<*(&LERpins+1)-LERpins; i++){pinMode(LERpins[i], INPUT);}
-  for(int i; i<*(&LEDpins+1)-LEDpins; i++){pinMode(LEDpins[i], OUTPUT);}
+  for(int i; i<2; i++){pinMode(LERpins[i], INPUT);}
+  for(int i; i<2; i++){pinMode(LEDpins[i], OUTPUT);}
 
-  lcd.init();
-  lcd.backlight();
-
+  lcdA.init();
+  lcdA.backlight();
+  lcdB.init();
+  lcdB.backlight();
+  
   digitalWrite(13, HIGH);
+  digitalWrite(12, HIGH);
 
   Serial.begin(9600);
   
 }
 
-void write(int cA, int cB) {
+void write(LiquidCrystal_I2C lcd, int cA, int cB, int C) {
   lcd.setCursor(5, 0);
   lcd.print(cA);
   lcd.setCursor(7, 0);
   lcd.print(':');
   lcd.setCursor(9, 0);
   lcd.print(cB);
+  lcd.setCursor(6, 1);
+  lcd.print(C);
 }
 
 int read(int pin, int counter){
 
     if (analogRead(pin) < umgebung/2) {
-      if (timeSinceGoal > 500) {
+      if (timeSinceGoal == 300) {
         timeSinceGoal = 0;
-        lcd.clear();
+        lcdA.clear();
+        lcdB.clear();
        return (counter+1);
        }
     }
@@ -69,8 +75,9 @@ void loop() {
   
  counterA = read(LERpins[A], counterA);
  counterB = read(LERpins[B], counterB);
- write(counterA, counterB);
+ write(lcdA, counterA, counterB, timeSinceGoal);
+ write(lcdB, counterA, counterB, timeSinceGoal);
   checkreset();
-  timeSinceGoal++;
+  if (timeSinceGoal<300) timeSinceGoal++;
   Serial.println((analogRead(LERpins[A]) < umgebung/2));
 }
